ebad4a84b4ec7ad8b972c9eb286d2f867d910029
St4r-L0rd <dylanvandijk7@gmail.com>
2025-05-02 20:36:12 +0800

    Switch to PostgreSQL backend

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.secret_key = os.environ.get("SECRET_KEY", "dev-fallback-key")
 
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///swiftdesk.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
